a4e6fedea4b0f91a8e4ea3cf056cd39622bd9efa
Nekopath <TheNekopath@gmail.com>
2023-11-06 18:09:15 +0100

    Add AggregateError and ExceptionError

---
+++ Setsu.Results/Errors/AggregateError.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Setsu.Results.Errors
+{
+    public sealed class AggregateError(IError[] errors, string message) : IError
+    {
+        public AggregateError(params IError[] errors) : this(errors, BuildMessage(errors)) { }
+        public IError[] Errors { get; } = errors;
+        public string Message { get => message; }
+
+        private static string BuildMessage(IError[] errors)
+        {
+            var sb = new StringBuilder("An aggregate error occurred.");
+            sb.AppendLine();
+            foreach (var error in errors)
+            {
+                sb.AppendLine("- " + error.Message);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Setsu.Results/Errors/ExceptionError.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Setsu.Results.Errors
+{
+    public sealed class ExceptionError(Exception ex) : IError
+    {
+        public Exception Exception { get; } = ex;
+        public string Message { get => Exception.Message; }
+    }
+}
